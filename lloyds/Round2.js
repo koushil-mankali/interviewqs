6f557ebbf9122ad391a4fe2ad628e47e35b29288
koushil <koushilmankali@gmail.com>
2025-06-02 17:33:00 +0530

    Infinite scroll code added

--- lloyds/Round2.js
+++ lloyds/Round2.js
@@ -1,3 +1,73 @@
 /**
  * QS: Write a react app which fetches data from api and displays on screen, and when user scrolls down to 90% of the page, it fetches more data and appends it to the existing data.
  */
+
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const fetchAPI = async ({ limit, skip }) => {
+  const data = await fetch(
+    `https://dummyjson.com/users?skip=${skip}&limit=${+limit}`
+  );
+  const parsedData = await data?.json();
+  return parsedData;
+};
+
+function App() {
+  const loadMoreRef = useRef(null);
+  const [users, setUsers] = useState([]);
+  const skip = useRef(0);
+  useEffect(() => {
+    (async () => {
+      const data = await fetchAPI({ limit: 30, skip: skip.current });
+      console.log("data", data);
+      setUsers((prev) => [...prev, ...data?.users]);
+    })();
+  }, []);
+
+  const refetch = async () => {
+    const data = await fetchAPI({
+      limit: 30,
+      skip: skip.current,
+    });
+    setUsers((prev) => [...prev, ...data?.users]);
+    skip.current += 30;
+  };
+
+  useEffect(() => {
+    (async () => {
+      const instOb = new IntersectionObserver(
+        async (entries) => {
+          console.log("entries", entries[0].isIntersecting);
+          if (entries[0].isIntersecting) {
+            await refetch();
+          }
+        },
+        { threshold: 0.25 }
+      );
+
+      instOb.observe(loadMoreRef.current);
+    })();
+  }, []);
+
+  return (
+    <>
+      {users?.map((item, idx) => {
+        return (
+          <div key={item.lastName + idx}>
+            <div style={{ margin: "10px 0" }}>
+              {item?.id}{" "}
+              {item?.firstName + " " + item?.maidenName + " " + item?.lastName}
+              <img src={item?.image} width="50px" height="50px" />
+            </div>
+          </div>
+        );
+      })}
+      <div id="loadmore" ref={loadMoreRef}>
+        LoadMore
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
